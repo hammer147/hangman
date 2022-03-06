ad507889e5f0d3e3f5ea7496908a15fe0ddea705
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2022-03-06 13:01:54 +0100

    added fullScreenCheck

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,12 +26,29 @@ const Home: NextPage = () => {
   const [wrongLetters, setWrongLetters] = useState<Letter[]>([])
   const [modalText, setModalText] = useState('')
 
+
+
+
+  function fullScreenCheck() {
+    if (document.fullscreenElement) return
+    return document.documentElement.requestFullscreen()
+  }
+
   useEffect(() => {
-    screen.orientation.lock('portrait')
-      .then(()=> alert('Locked to portrait'))
-      .catch(error => alert(error))
+    (async () => {
+      try {
+        await fullScreenCheck()
+      } catch (err) {
+        alert(err)
+      }
+      screen.orientation.lock('portrait')
+        .then(() => alert('Locked to portrait'))
+        .catch(error => alert(error))
+    })()
   }, [])
-  
+
+
+
 
   const reset = () => {
     setSelectedWord(() => words[Math.floor(Math.random() * words.length)])
